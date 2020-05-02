4c4de73188c647165949bb9090ae08f3d555cc6e
QL31 <qinglian.li1070@gmail.com>
2020-05-02 16:29:34 +0200

    Making cell swipeable

--- NoteBook/ViewController/NoteBookTableViewController.swift
+++ NoteBook/ViewController/NoteBookTableViewController.swift
@@ -8,30 +8,41 @@
 
 import UIKit
 import RealmSwift
+import SwipeCellKit
 
 class NoteBookTableViewController: UITableViewController {
     
     let realm = try! Realm()
     
     var categories : Results<Categorie>?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadCategori()
-
+        tableView.rowHeight = 80.0
+        
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return categories?.count ?? 1
     }
     
+    
+    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
+    //        cell.delegate = self
+    //        return cell
+    //    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=tableView.dequeueReusableCell(withIdentifier: "categorieCell", for: indexPath)
+        
+        let cell=tableView.dequeueReusableCell(withIdentifier: "categorieCell", for: indexPath) as! SwipeTableViewCell
+        
+        cell.delegate = self
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categorie added yet"
-      
+        
         return cell
     }
     
@@ -48,7 +59,7 @@ class NoteBookTableViewController: UITableViewController {
         }
     }
     
-
+    
     
     
     @IBAction func addCategorie(_ sender: UIBarButtonItem) {
@@ -83,7 +94,7 @@ class NoteBookTableViewController: UITableViewController {
         }catch{
             print("Error adding category \(error)")
         }
-            
+        
         tableView.reloadData()
     }
     func loadCategori(){
@@ -92,6 +103,43 @@ class NoteBookTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-     
+    
+    
+}
+// MARK: swipe table view delegate
 
+extension NoteBookTableViewController: SwipeTableViewCellDelegate{
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            print("item delete")
+            // handle action by updating model with deletion
+            if let item = self.categories?[indexPath.row]{
+                do{
+                    try self.realm.write{
+                        self.realm.delete(item)
+                    }
+                }catch{
+                    print("Error deleting categorie data \(error)")
+                }
+                //tableView.reloadData()
+                
+            }
+            
+        }
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "Delete-Icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+       // options.transitionStyle = .border
+        return options
+    }
 }
